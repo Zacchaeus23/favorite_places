70a01880035e001d94241bd8560e227dc21de645
Michael Zacchaeus <108746426+Zacchaeus23@users.noreply.github.com>
2024-07-09 11:44:21 +0100

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -29,7 +31,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(child: MyApp()),
   );
 }
 
